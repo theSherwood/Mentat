eb2940b138d164444f09749bf082b493b43e4b0b
admls <adam.the.sherwood@gmail.com>
2019-02-04 17:08:46 +0000

    positions resizers accurately, though not yet initialized correctly

--- src/tiddlers/system/plugins/admls/mentat/globals/fakeName.js
+++ src/tiddlers/system/plugins/admls/mentat/globals/fakeName.js
@@ -3,7 +3,7 @@ created: 20190201185751112
 type: application/javascript
 title: $:/plugins/admls/mentat/globals/fakeName.js
 tags: unfinished tampered
-modified: 20190202002503634
+modified: 20190204170500570
 module-type: global
 
 Description...
@@ -16,7 +16,7 @@ ToDo:
 - edit doesn't work with zStack for some reason
 - edit doesn't run macro. So a movingtiddler that is opened in edit mode first may not get any of the eventhandlers attached. I'm not sure.
 - I may have introduced problem with getEventTiddler and the way it affects the zStack with window-tiddlers
-
+- initialize dimension fields
 
 \*/
 
@@ -65,6 +65,11 @@ const Weird = {
         const top = tiddler.offsetTop;
         const left = tiddler.offsetLeft;
         
+        // get resizers
+        const resizerLeft = tiddler.querySelector(".resizer-left");
+        const resizerRight = tiddler.querySelector(".resizer-right");
+        
+        /*\
         // set the element's new position: prevent them from
         // running off the window (assumes fixed position)
         if (tiddler.style.position === "fixed") {
@@ -73,12 +78,18 @@ const Weird = {
             };
             if (tiddler.offsetLeft - Weird.pos1 >= 0 && window.innerWidth >= tiddler.offsetLeft - Weird.pos1 + tiddler.offsetWidth) {
                 tiddler.style.left = (left - Weird.pos1) + "px";
-            };
-        
+            };  
         } else {
+        \*/
+        
         	tiddler.style.top = (top - Weird.pos2) + "px";
             tiddler.style.left = (left - Weird.pos1) + "px";
-        }
+            
+            resizerLeft.style.top = (top + tiddler.offsetHeight - Weird.pos2 - resizerLeft.offsetHeight) + "px";
+            resizerLeft.style.left = tiddler.style.left;
+            resizerRight.style.top = (top + tiddler.offsetHeight - Weird.pos2 - resizerRight.offsetHeight) + "px";
+            resizerRight.style.left = (left - Weird.pos1 + tiddler.offsetWidth - resizerRight.offsetWidth) + "px";
+        //}
     },
 
 	endDrag: function() {
@@ -97,8 +108,14 @@ const Weird = {
         $tw.wiki.setText(title,'left',undefined,(tiddler.offsetLeft)+"px",undefined);
         $tw.wiki.setText(title,'width',undefined,(tiddler.offsetWidth)+"px",undefined);
         $tw.wiki.setText(title,'height',undefined,(tiddler.offsetHeight)+"px",undefined);
-        $tw.wiki.setText(title,'bottom',undefined,(tiddler.offsetTop+tiddler.offsetHeight)+"px",undefined);
-        $tw.wiki.setText(title,'right',undefined,(tiddler.offsetLeft+tiddler.offsetWidth)+"px",undefined);
+        // Log resizer-left position
+        const resizerLeft = tiddler.querySelector(".resizer-left");
+        $tw.wiki.setText(title,'resizerleft-top',undefined,(tiddler.offsetTop+tiddler.offsetHeight-resizerLeft.offsetHeight)+"px",undefined);
+        $tw.wiki.setText(title,'resizerleft-left',undefined,(tiddler.offsetLeft)+"px",undefined);
+        // Log resizer-Right position
+        const resizerRight = tiddler.querySelector(".resizer-right");
+        $tw.wiki.setText(title,'resizerright-top',undefined,(tiddler.offsetTop+tiddler.offsetHeight-resizerRight.offsetHeight)+"px",undefined);
+        $tw.wiki.setText(title,'resizerright-left',undefined,(tiddler.offsetLeft+tiddler.offsetWidth-resizerRight.offsetWidth)+"px",undefined);
         // Wait to get rid of element styles until the fields have been updated
         setTimeout(function() {
         	tiddler.style.top = "";
@@ -156,15 +173,25 @@ const Weird = {
 
 	resizeLeft: function(e) {
     	const tiddler = $tw.Weird.eventTiddler;
+        const resizerLeft = tiddler.querySelector(".resizer-left");
+        const resizerRight = tiddler.querySelector(".resizer-right");
         tiddler.style.width = (tiddler.offsetWidth + (tiddler.offsetLeft - e.clientX) + 5) + 'px';
         tiddler.style.left = (e.clientX - 5) + 'px';
        	tiddler.style.height = (e.clientY - tiddler.offsetTop + 5) + 'px';
+        resizerLeft.style.top = (e.clientY + 5 - resizerLeft.offsetHeight) + 'px';
+        resizerLeft.style.left = tiddler.style.left;
+        resizerRight.style.top = (e.clientY + 5 - resizerRight.offsetHeight) + 'px';
     },
     
     resizeRight: function(e) {
         const tiddler = $tw.Weird.eventTiddler;
+        const resizerLeft = tiddler.querySelector(".resizer-left");
+        const resizerRight = tiddler.querySelector(".resizer-right");
        	tiddler.style.width = (e.clientX - tiddler.offsetLeft + 5) + 'px';
-       	tiddler.style.height = (e.clientY - tiddler.offsetTop + 5) + 'px';      	
+       	tiddler.style.height = (e.clientY - tiddler.offsetTop + 5) + 'px';
+        resizerRight.style.top = (e.clientY + 5 - resizerRight.offsetHeight) + 'px';
+        resizerRight.style.left = (e.clientX + 5 - resizerRight.offsetWidth) + 'px';
+        resizerLeft.style.top = (e.clientY + 5 - resizerLeft.offsetHeight) + 'px';
     },
     
     endResize: function() {
@@ -186,6 +213,10 @@ const Weird = {
             elmnt = elmnt.parentElement;
         }
         return elmnt;
+    },
+    
+    dragResizers: function(tiddler) {
+    	const resizerLeft = tiddler.querySelector(".resizerLeft");
     }
 
 };    
