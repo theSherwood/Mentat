bdbb208f5a65047c7e87444cace6ec48cb73cc95
admls <adam.the.sherwood@gmail.com>
2019-03-07 00:19:17 +0000

    removes animation and fixes initialization bug

--- src/tiddlers/system/plugins/admls/mentat/storyviews/mentat.js
+++ src/tiddlers/system/plugins/admls/mentat/storyviews/mentat.js
@@ -3,10 +3,10 @@ created: 20190129200505951
 type: application/javascript
 title: $:/plugins/admls/mentat/storyviews/mentat.js
 tags: 
-modified: 20190227183550720
+modified: 20190306230040860
 module-type: storyview
 
-Views the story as a collection of story-windows
+Views the story as a collection of story windows
 
 \*/
 (function () {
@@ -14,8 +14,6 @@ Views the story as a collection of story-windows
 	/*jslint node: true, browser: true */
 	/*global $tw: false */
 	"use strict";
-
-	var easing = "cubic-bezier(0.645, 0.045, 0.355, 1)";
 
 	var MentatStoryView = function (listWidget) {
 		this.listWidget = listWidget;
@@ -31,9 +29,26 @@ Views the story as a collection of story-windows
 			const tiddlerTitle = itemWidget.parseTreeNode.itemTitle;
 			const tiddler = $tw.wiki.getTiddler(tiddlerTitle);
 			// If the tiddler is not tagged with $:/MentatMenu or $:/Window
-			if (tiddler && (!tiddler.fields.tags || !(tiddler.fields.tags.includes("$:/MentatMenu") || tiddler.fields.tags.includes("$:/Window")))) {
+			// if (tiddler && (!tiddler.fields.tags || !(tiddler.fields.tags.includes("$:/MentatMenu") || tiddler.fields.tags.includes("$:/Window")))) {
+			// 	domNode.style.display = "none";
+			// }
+
+			const M = $tw.Mentat;
+			M.updateAllowedTags();
+
+			// Check to see if tiddler has allowed tags
+			for (let tag of M.allowedTags) {
+				if (tiddler && tiddler.fields.tags && tiddler.fields.tags.includes(tag)) {
+					return;
+				}
+			}
+
+			if (tiddler) {
 				domNode.style.display = "none";
 			}
+
+
+
 		});
 
 	};
@@ -55,8 +70,7 @@ Views the story as a collection of story-windows
 	};
 
 	MentatStoryView.prototype.insert = function (widget) {
-		var domNode = widget.findFirstDomNode(),
-			duration = $tw.utils.getAnimationDuration();
+		var domNode = widget.findFirstDomNode();
 		// Abandon if the list entry isn't a DOM element (it might be a text node)
 		if (!(domNode instanceof Element)) {
 			return;
@@ -99,44 +113,7 @@ Views the story as a collection of story-windows
 	};
 
 	MentatStoryView.prototype.remove = function (widget) {
-		var targetElement = widget.findFirstDomNode(),
-			duration = $tw.utils.getAnimationDuration(),
-			removeElement = function () {
-				widget.removeChildDomNodes();
-			};
-		// Abandon if the list entry isn't a DOM element (it might be a text node)
-		if (!(targetElement instanceof Element)) {
-			removeElement();
-			return;
-		}
-		// Get the current height of the tiddler
-		var currWidth = targetElement.offsetWidth,
-			computedStyle = window.getComputedStyle(targetElement),
-			currMarginBottom = parseInt(computedStyle.marginBottom, 10),
-			currMarginTop = parseInt(computedStyle.marginTop, 10),
-			currHeight = targetElement.offsetHeight + currMarginTop;
-		// Remove the dom nodes of the widget at the end of the transition
-		setTimeout(removeElement, duration);
-		// Animate the closure
-		$tw.utils.setStyle(targetElement, [
-			{ transition: "none" },
-			{ transform: "translateX(0px)" },
-			{ marginBottom: currMarginBottom + "px" },
-			{ opacity: "1.0" }
-		]);
-		$tw.utils.forceLayout(targetElement);
-		$tw.utils.setStyle(targetElement, [
-			{
-				transition: $tw.utils.roundTripPropertyName("transform") + " " + duration + "ms " + easing + ", " +
-					"opacity " + duration + "ms " + easing + ", " +
-					"margin-bottom " + duration + "ms " + easing
-			},
-			{ transform: "translateX(-" + currWidth + "px)" },
-			{ marginBottom: (-currHeight) + "px" },
-			{ opacity: "0.0" }
-		]);
-
-
+		widget.removeChildDomNodes();
 	};
 
 	exports.mentat = MentatStoryView;
