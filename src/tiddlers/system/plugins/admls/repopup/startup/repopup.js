e0bd6e82f8c2dfbdce5319deb163c72b2c0d5210
admls <adam.the.sherwood@gmail.com>
2019-03-09 23:59:44 +0000

    fixes bottom overflow bug

--- src/tiddlers/system/plugins/admls/repopup/startup/repopup.js
+++ src/tiddlers/system/plugins/admls/repopup/startup/repopup.js
@@ -17,10 +17,32 @@ const revealWidget = require("$:/core/modules/widgets/reveal.js").reveal;
 
 revealWidget.prototype.positionPopup = function(domNode) {
 	domNode.style.position = "absolute";
-	domNode.style.zIndex = "1000";
-    
+    domNode.style.zIndex = "1000";
+
+    /* 
+    Position the domNode relative to the parentElement.
+    This can help avoid weird scrolling stuff.
+    */
+    const parent = domNode.parentElement;
+    const parentPosition = getComputedStyle(parent).position;
+    if (parentPosition !== "absolute" && parentPosition !== "fixed") {
+        parent.style.position = "relative";
+    }
+
+    /* 
+    The previousElementSibling is presumably the button that triggers the popup.
+    this.popup is the location of this element. It needs updated because we are
+    now making everything relative to the parentElement.
+    */
+    const sibling = domNode.previousElementSibling;
+    this.popup.left = sibling.offsetLeft;
+    this.popup.top = sibling.offsetTop;
+
+    /* 
+    This is unchanged from the native version of the revealWidget. Though we have
+    changed the value of the this.popup.left and this.popup.top.
+    */
     if(this.popup) {
-    // Set the popup position as usual
 	switch(this.position) {
 		case "left":
 			domNode.style.left = Math.max(0, this.popup.left - domNode.offsetWidth) + "px";
@@ -52,7 +74,6 @@ revealWidget.prototype.positionPopup = function(domNode) {
    	/*
     Adjust the native popup position so that it fits in the viewport
     */
-    
     const viewportHeight = document.documentElement.clientHeight;
     const viewportWidth = document.documentElement.clientWidth;
    	const nodePosition = domNode.getBoundingClientRect();
@@ -98,27 +119,43 @@ revealWidget.prototype.positionPopup = function(domNode) {
 
     const buffer = 20;
 
-    if(position.bottom > viewportHeight - buffer) { // At the bottom
-        let newHeight = viewportHeight - buffer - Number(domNode.style.top.slice(0,-2));
+    // Shift away from overflowing the screen in height if necessary
+    if(position.bottom > viewportHeight - buffer) {
+        let newHeight = viewportHeight - buffer - position.top;
         domNode.style.height = newHeight + "px";
+        domNode.style.cssText += "overflow: auto;";
     }
     // Shift away from overflowing the screen in width 
     if(position.left < buffer) { // At the left
     	position.left = buffer;
         position.right += buffer;
     }
+
+    // Rewrite?
   	if(position.right > viewportWidth - buffer) { // At the right
     	let newWidth = position.width;
     	if(position.width > viewportWidth - 2 * buffer) {
-        	newWdith = viewportWidth - 2 * buffer;
+            newWidth = viewportWidth - 2 * buffer;
         	domNode.style.width = newWidth + "px";
         }
         if(position.left + newWidth > viewportWidth - buffer) {
+            console.log(Number(domNode.style.left.slice(0,-2)));
+            console.log(position.left);
         	const oldLeft = Number(domNode.style.left.slice(0,-2));
             const differenceLeft = position.left + newWidth - viewportWidth + buffer;
             domNode.style.left = oldLeft - differenceLeft + "px";
+            domNode.style.cssText += "overflow: auto;";
         }
-    }   
+    }  
+
+    /*
+    Now that we know where it should be positioned, make it "fixed"
+    so that it clears any overflow restrictions on parent elements.
+    */
+    const viewportOffset = domNode.getBoundingClientRect();
+    domNode.style.position = "fixed";
+    domNode.style.top = viewportOffset.top + "px";
+    domNode.style.left = viewportOffset.left + "px";
 };
 
 })();
